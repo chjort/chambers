dc170af2798bd1a9f4fbd157b84c2f18d991b9e3
chjort <christoffer.hjort1995@gmail.com>
2020-03-09 22:51:08 +0100

    Refactor contrasive losses

--- losses/contrastive.py
+++ losses/contrastive.py
@@ -7,40 +7,54 @@ class PairBasedLoss(tf.keras.losses.Loss):
     def __init__(self, name):
         super().__init__(name=name)
 
+    # def call(self, y_true, y_pred):
+    #     # todo: compute similarity, get pos/neg pairs, and mine
+    #     pass
+
     @abc.abstractmethod
     def compute_similarity_matrix(self, y_true, y_pred):
         pass
 
-    def get_signed_pairs(self, sim_mat, y_true, remove_positive_diag=True):
+    def get_signed_pairs(self, similarity_matrix, y_true, remove_positive_diag=True):
         y_true = tf.reshape(y_true, [-1, 1])
         pos_pair_mask = tf.equal(y_true, tf.transpose(y_true))
         neg_pair_mask = tf.logical_not(pos_pair_mask)
 
         if remove_positive_diag:
-            # TODO: Dont remove mirror pairs by removing diagonal. This is wrong, if similarity matrix is not square
             # remove mirror pairs
-            diag_len = tf.shape(sim_mat)[0]
+            diag_len = tf.shape(similarity_matrix)[0]
             pos_pair_mask = tf.linalg.set_diag(pos_pair_mask, tf.tile([False], [diag_len]))
 
         # get similarities for positive pairs
-        pos_mat = tf.RaggedTensor.from_row_lengths(values=sim_mat[pos_pair_mask],
+        pos_mat = tf.RaggedTensor.from_row_lengths(values=similarity_matrix[pos_pair_mask],
                                                    row_lengths=tf.reduce_sum(tf.cast(pos_pair_mask, tf.int32), axis=1)
                                                    )
 
         # get similarities for negative pairs
-        neg_mat = tf.RaggedTensor.from_row_lengths(values=sim_mat[neg_pair_mask],
+        neg_mat = tf.RaggedTensor.from_row_lengths(values=similarity_matrix[neg_pair_mask],
                                                    row_lengths=tf.reduce_sum(tf.cast(neg_pair_mask, tf.int32), axis=1)
                                                    )
 
         return pos_mat, neg_mat
 
+    def _remove_diag(self, mat):
+        pass
+
 
 class MultiSimilarityLoss(PairBasedLoss):
+    """
+    Multi-similarity loss
+
+    References:
+    [1] Wang, Xun et al. “Multi-Similarity Loss With General Pair Weighting for Deep Metric Learning.”
+        2019 IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) (2019): 5017-5025.
+        https://arxiv.org/abs/1904.06627
+    """
     def __init__(self, pos_scale=2.0, neg_scale=40.0, threshold=0.5, miner=_MSMiner(margin=0.1), name="multi_similarity_loss"):
         super().__init__(name=name)
-        self.pos_scale = pos_scale
-        self.neg_scale = neg_scale
-        self.threshold = threshold
+        self.pos_scale = pos_scale  # alpha
+        self.neg_scale = neg_scale  # beta
+        self.threshold = threshold  # lambda
         self.miner = miner
 
     def call(self, y_true, y_pred):
--- utils/ranking.py
+++ utils/ranking.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 from .tf import arg_to_gather_nd
 
 
-def score_matrix_to_binary_ranking(score_matrix, query_labels, all_labels, remove_top1=False):
+def score_matrix_to_binary_ranking(similarity_matrix, query_labels, candidate_labels, remove_top1=False):
     query_labels = tf.reshape(query_labels, [-1, 1])
-    all_labels = tf.reshape(all_labels, [-1, 1])
-    pair_signs = tf.cast(tf.equal(query_labels, tf.transpose(all_labels)), tf.float32)
+    candidate_labels = tf.reshape(candidate_labels, [-1, 1])
+    pair_signs = tf.cast(tf.equal(query_labels, tf.transpose(candidate_labels)), tf.float32)
 
-    index_ranking = tf.argsort(score_matrix, axis=1, direction="DESCENDING")
+    index_ranking = tf.argsort(similarity_matrix, axis=1, direction="DESCENDING")
     if remove_top1:
         index_ranking = index_ranking[:, 1:]
 

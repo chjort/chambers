fb395ad07e1bae2e2c4affead94e9dd759306905
chjort <christoffer.hjort1995@gmail.com>
2020-04-02 11:08:07 +0200

    Refactor miner class

--- miners.py
+++ miners.py
@@ -14,11 +14,23 @@ class Miner(ABC):
         graph_ctx = tf_utils.graph_context_for_symbolic_tensors(
             positive, negative)
         with K.name_scope(scope_name or self.__class__.__name__), graph_ctx:
-            return self.call(positive, negative)
+            return self.mine(positive, negative)
 
     @abstractmethod
-    def call(self, positive, negative):
+    def compute_masks(self, positive, negative):
         pass
+
+    @staticmethod
+    def apply_masks(positive, negative, positive_mask, negative_mask):
+        positive = tf.ragged.boolean_mask(positive, positive_mask)
+        negative = tf.ragged.boolean_mask(negative, negative_mask)
+        return positive, negative
+
+    def mine(self, positive, negative):
+        mined_pos_mask, mined_neg_mask = self.compute_masks(positive, negative)
+        mined_pos_mat, mined_neg_mat = self.apply_masks(positive, negative, mined_pos_mask, mined_neg_mask)
+
+        return mined_pos_mat, mined_neg_mat
 
 
 class MultiSimilarityMiner(Miner):
@@ -26,14 +38,11 @@ class MultiSimilarityMiner(Miner):
         super().__init__(name=name)
         self.margin = margin
 
-    def call(self, positive, negative):
+    def compute_masks(self, positive, negative):
         pos_thresh = tf.reduce_max(negative, axis=1) + self.margin
         neg_thresh = tf.reduce_min(positive, axis=1) - self.margin
 
         mined_pos_mask = positive < tf.reshape(pos_thresh, [-1, 1])
         mined_neg_mask = negative > tf.reshape(neg_thresh, [-1, 1])
 
-        mined_pos_mat = tf.ragged.boolean_mask(positive, mined_pos_mask)
-        mined_neg_mat = tf.ragged.boolean_mask(negative, mined_neg_mask)
-
-        return mined_pos_mat, mined_neg_mat
+        return mined_pos_mask, mined_neg_mask

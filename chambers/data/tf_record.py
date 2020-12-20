d471180f7e79807380637285edf818dbfaa90311
chjort <christoffer.hjort1995@gmail.com>
2020-12-20 17:18:29 +0100

    Keep shape information for tfrecord

--- chambers/data/tf_record.py
+++ chambers/data/tf_record.py
@@ -3,19 +3,25 @@ import tensorflow as tf
 
 def _float_feature(value):
     """Returns a float_list from a float / double."""
-    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+    if tf.rank(value) == 0:
+        value = [value]
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
 
 def _int_feature(value):
     """Returns an int64_list from a bool / enum / int / uint."""
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    if tf.rank(value) == 0:
+        value = [value]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 
 def _bytes_feature(value):
     """Returns a bytes_list from a byte value."""
     if isinstance(value, type(tf.constant(0))):
         value = value.numpy()  # BytesList won't unpack a string from an EagerTensor.
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    if tf.rank(value) == 0:
+        value = [value]
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
 
 
 def _get_tensor_ids(dictionary):
@@ -34,6 +40,7 @@ def _make_feature(tensors):
         name = "t" + str(i)
 
         dtype = t.dtype
+        shape = tf.shape(t)
 
         if t.shape != ():  # if tensor is not a scalar, we have to serialize it.
             t = tf.io.serialize_tensor(t)
@@ -52,6 +59,7 @@ def _make_feature(tensors):
 
         feature[name + "_raw"] = raw_feature
         feature[name + "_dtype"] = _int_feature(dtype.as_datatype_enum)
+        feature[name + "_shape"] = _int_feature(shape)
 
     return feature
 
@@ -63,6 +71,7 @@ def _make_description(feature):
     for tn in tensors_ids:
         is_serialized = feature[tn + "_serialized"].int64_list.value[0]
         dtype = feature[tn + "_dtype"].int64_list.value[0]
+        shape = feature[tn + "_shape"].int64_list.value
 
         description[tn + "_serialized"] = tf.io.FixedLenFeature([], tf.int64)
         if is_serialized:
@@ -71,6 +80,7 @@ def _make_description(feature):
             description[tn + "_raw"] = tf.io.FixedLenFeature([], tf.as_dtype(dtype))
 
         description[tn + "_dtype"] = tf.io.FixedLenFeature([], tf.int64)
+        description[tn + "_shape"] = tf.io.FixedLenFeature([len(shape)], tf.int64)
 
     return description
 
@@ -91,24 +101,36 @@ def serialize_example(*args):
     return tf.reshape(example, [])
 
 
-def _make_feature_deserialize_fn(feature):
+def _make_feature_deserialize_fn(feature, set_shape=False, set_size=False):
     description = _make_description(feature)
     tensor_ids = _get_tensor_ids(feature)
-    dtypes = [
-        tf.as_dtype(feature[tn + "_dtype"].int64_list.value[0]) for tn in tensor_ids
-    ]
     is_serialized = [
         feature[tn + "_serialized"].int64_list.value[0] for tn in tensor_ids
     ]
+    dtypes = [
+        tf.as_dtype(feature[tn + "_dtype"].int64_list.value[0]) for tn in tensor_ids
+    ]
+    shapes = [feature[tn + "_shape"].int64_list.value for tn in tensor_ids]
 
     def deserialize_fn(x):
         tensor_example = tf.io.parse_example(x, description)
         tensors = []
-        for tn, dtype, serialized in zip(tensor_ids, dtypes, is_serialized):
+        for i, tn in enumerate(tensor_ids):
             name = tn + "_raw"
+            serialized = is_serialized[i]
+            dtype = dtypes[i]
+
             t = tensor_example[name]
             if serialized:
                 t = tf.io.parse_tensor(t, out_type=dtype)
+
+            if set_shape:
+                shape = shapes[i]
+                t.set_shape(shape)
+            elif set_size:
+                shape = tensor_example[tn + "_shape"]
+                t = tf.reshape(t, shape)
+
             tensors.append(t)
 
         if len(tensors) == 1:
@@ -121,9 +143,22 @@ def _make_feature_deserialize_fn(feature):
     return deserialize_fn
 
 
-def make_dataset_deserialize_fn(dataset):
+def make_dataset_deserialize_fn(dataset, set_shape=False, set_size=False):
     sample = next(iter(dataset))
 
     example_features = tf.train.Example.FromString(sample.numpy())
     feature = example_features.features.feature
-    return _make_feature_deserialize_fn(feature)
+    return _make_feature_deserialize_fn(feature, set_shape=set_shape, set_size=set_size)
+
+
+def dataset_to_tfrecord(dataset, path):
+    dataset = dataset.map(serialize_example)
+
+    writer = tf.data.experimental.TFRecordWriter(path)
+    writer.write(dataset)
+
+
+def tfrecord_to_dataset(paths) -> tf.data.Dataset:
+    td = tf.data.TFRecordDataset(paths)
+    td = td.map(make_dataset_deserialize_fn(td))
+    return td

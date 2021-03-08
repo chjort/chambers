ec9e7ec68c8f617843855bc06ab94953be020912
chjort <christoffer.hjort1995@gmail.com>
2021-03-08 19:56:55 +0100

    writing dataset to multiple tfrecord files

--- chambers/data/tf_record.py
+++ chambers/data/tf_record.py
@@ -1,3 +1,6 @@
+import os
+import glob
+
 import tensorflow as tf
 
 
@@ -118,22 +121,43 @@ def _make_feature_deserialize_fn(feature, set_shape=False, set_dimension=False):
     return deserialize_fn
 
 
-def make_dataset_deserialize_fn(dataset, set_shape=False, set_dimension=False) -> callable:
+def make_dataset_deserialize_fn(
+    dataset, set_shape=False, set_dimension=False
+) -> callable:
     sample = next(iter(dataset))
 
     example_features = tf.train.Example.FromString(sample.numpy())
     feature = example_features.features.feature
-    return _make_feature_deserialize_fn(feature, set_shape=set_shape, set_dimension=set_dimension)
+    return _make_feature_deserialize_fn(
+        feature, set_shape=set_shape, set_dimension=set_dimension
+    )
 
 
-def dataset_to_tfrecord(dataset, path):
+def dataset_to_tfrecord(dataset, path, n_files=1):
     dataset = dataset.map(serialize_to_example)
 
-    writer = tf.data.experimental.TFRecordWriter(path)
-    writer.write(dataset)
+    # make directory for record files
+    record_dir, record_name = os.path.split(path)
+    if record_dir != "":
+        os.makedirs(record_dir, exist_ok=True)
+
+    # shard dataset and write files
+    for i in range(n_files):
+        shard = dataset.shard(n_files, i)
+        path_i = os.path.join(
+            record_dir, "{}.data-{:05d}-of-{:05d}".format(record_name, i + 1, n_files)
+        )
+        writer = tf.data.experimental.TFRecordWriter(path_i)
+        writer.write(shard)
 
 
-def tfrecord_to_dataset(paths, set_shape=True, set_dimension=False) -> tf.data.Dataset:
-    td = tf.data.TFRecordDataset(paths)
-    td = td.map(make_dataset_deserialize_fn(td, set_shape=set_shape, set_dimension=set_dimension))
+def tfrecord_to_dataset(
+    filenames, set_shape=True, set_dimension=False, num_parallel_reads=None
+) -> tf.data.Dataset:
+    td = tf.data.TFRecordDataset(filenames, num_parallel_reads=num_parallel_reads)
+    td = td.map(
+        make_dataset_deserialize_fn(
+            td, set_shape=set_shape, set_dimension=set_dimension
+        )
+    )
     return td

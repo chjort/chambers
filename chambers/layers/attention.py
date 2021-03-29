fix attention mask value

chambers/layers/attention.py
@@ -36,7 +36,9 @@ class Attention(tf.keras.layers.Attention):
         if scores_mask is not None:
             padding_mask = math_ops.logical_not(scores_mask)
             # Bias so padding positions do not contribute to attention distribution.
-            scores -= tf.minimum(scores.dtype.max, 1e-9) * math_ops.cast(padding_mask, dtype=scores.dtype)
+            scores -= tf.minimum(scores.dtype.max, 1.0e9) * math_ops.cast(
+                padding_mask, dtype=scores.dtype
+            )
         if training is None:
             training = tf.keras.backend.learning_phase()
         weights = tf.nn.softmax(scores)
@@ -71,7 +73,7 @@ class ScaledAttention(Attention):
 
 
 @tf.keras.utils.register_keras_serializable(package="Chambers")
-class MultiHeadAttentionEin(tf.keras.layers.Layer):
+class MultiHeadAttention(tf.keras.layers.Layer):
     def __init__(
         self,
         head_dim=64,
@@ -139,7 +141,7 @@ class MultiHeadAttentionEin(tf.keras.layers.Layer):
             shape=(1, d),
             initializer="zeros",
         )
-        super(MultiHeadAttentionEin, self).build(input_shape)
+        super(MultiHeadAttention, self).build(input_shape)
 
     def call(self, inputs, mask=None, training=None):
         """
@@ -207,7 +209,7 @@ class MultiHeadAttentionEin(tf.keras.layers.Layer):
             "dropout_rate": self.dropout_rate,
             "causal": self.causal,
         }
-        base_config = super(MultiHeadAttentionEin, self).get_config()
+        base_config = super(MultiHeadAttention, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
     def from_config(cls, config):
@@ -215,130 +217,3 @@ class MultiHeadAttentionEin(tf.keras.layers.Layer):
             config["dense_kernel_initializer"]
         )
         return cls(**config)
-
-
-MultiHeadAttention = MultiHeadAttentionEin
-
-# class MultiHeadAttention(tf.keras.layers.Layer):
-#     def __init__(
-#         self,
-#         embed_dim=512,
-#         num_heads=8,
-#         dense_kernel_initializer="glorot_uniform",
-#         dropout_rate=0.1,
-#         causal=False,
-#     ):
-#         super(MultiHeadAttention, self).__init__()
-#         self.embed_dim = embed_dim
-#         self.num_heads = num_heads
-#         self.dense_kernel_initializer = dense_kernel_initializer
-#         self.dropout_rate = dropout_rate
-#         self.causal = causal
-#
-#         assert embed_dim % num_heads == 0
-#         head_dim = embed_dim // num_heads
-#
-#         self.w_query = tf.keras.layers.Dense(
-#             embed_dim, kernel_initializer=dense_kernel_initializer
-#         )
-#         self.w_value = tf.keras.layers.Dense(
-#             embed_dim, kernel_initializer=dense_kernel_initializer
-#         )
-#         self.w_key = tf.keras.layers.Dense(
-#             embed_dim, kernel_initializer=dense_kernel_initializer
-#         )
-#         self.w_projection = tf.keras.layers.Dense(
-#             embed_dim, kernel_initializer=dense_kernel_initializer
-#         )
-#         self.attention = ScaledAttention(
-#             key_dim=head_dim, causal=causal, dropout=dropout_rate
-#         )
-#
-#         self.reshape_split = tf.keras.layers.Reshape((-1, num_heads, head_dim))
-#         self.permute = tf.keras.layers.Permute((2, 1, 3))
-#
-#         self.reshape_split_mask = tf.keras.layers.Reshape((-1, 1))
-#         self.permute_mask = tf.keras.layers.Permute((2, 1))
-#
-#         self.reshape_merge = tf.keras.layers.Reshape((-1, embed_dim))
-#
-#     def call(self, inputs, mask=None, training=None):
-#         q = inputs[0]  # [batch_size, tq, embed_dim]
-#         v = inputs[1]  # [batch_size, tv, embed_dim]
-#         k = inputs[2] if len(inputs) > 2 else v  # [batch_size, tv, embed_dim]
-#
-#         query = self.w_query(q)  # [batch_size, tq, embed_dim]
-#         query = self.separate_heads(query)  # [batch_size, num_heads, tq, head_dim]
-#
-#         value = self.w_value(v)  # [batch_size, tv, embed_dim]
-#         value = self.separate_heads(value)  # [batch_size, num_heads, tv, head_dim]
-#
-#         key = self.w_key(k)  # [batch_size, tv, embed_dim]
-#         key = self.separate_heads(key)  # [batch_size, num_heads, tv, head_dim]
-#
-#         if mask is not None:
-#             mask = self.separate_heads_mask(mask)
-#
-#         attention = self.attention(
-#             [query, value, key], mask=mask, training=training
-#         )  # [batch_size, num_heads, tq, head_dim]
-#         attention = self.merge_heads(attention)  # [batch_size, tq, embed_dim]
-#
-#         x = self.w_projection(attention)  # [batch_size, tq, embed_dim]
-#
-#         return x
-#
-#     def separate_heads(self, x):
-#         x = self.reshape_split(x)  # [batch_size, t, num_heads, head_dim]
-#         x = self.permute(x)  # [batch_size, num_heads, t, head_dim]
-#         return x
-#
-#     def separate_heads_mask(self, mask):
-#         query_mask = mask[0]  # [batch_size, tq]
-#         value_mask = mask[1]  # [batch_size, tv]
-#
-#         if query_mask is not None:
-#             query_mask = self.reshape_split_mask(
-#                 query_mask
-#             )  # [batch_size, tq, num_heads]
-#             query_mask = self.permute_mask(query_mask)  # [batch_size, num_heads, tq]
-#
-#         if value_mask is not None:
-#             value_mask = self.reshape_split_mask(
-#                 value_mask
-#             )  # [batch_size, tv, num_heads]
-#             value_mask = self.permute_mask(value_mask)  # [batch_size, num_heads, tv]
-#
-#         return [query_mask, value_mask]
-#
-#     def merge_heads(self, x):
-#         x = self.permute(x)  # [batch_size, t, num_heads, head_dim]
-#         x = self.reshape_merge(x)
-#         return x
-#
-#     def compute_mask(self, inputs, mask=None):
-#         if mask:
-#             q_mask = mask[0]
-#             if q_mask is None:
-#                 return None
-#             return tf.convert_to_tensor(q_mask)
-#         return None
-#
-#     def get_config(self):
-#         config = {
-#             "embed_dim": self.embed_dim,
-#             "num_heads": self.num_heads,
-#             "dense_kernel_initializer": tf.keras.initializers.serialize(
-#                 self.dense_kernel_initializer
-#             ),
-#             "dropout_rate": self.dropout_rate,
-#             "causal": self.causal,
-#         }
-#         base_config = super(MultiHeadAttention, self).get_config()
-#         return dict(list(base_config.items()) + list(config.items()))
-#
-#     def from_config(cls, config):
-#         config["dense_kernel_initializer"] = tf.keras.initializers.deserialize(
-#             config["dense_kernel_initializer"]
-#         )
-#         return cls(**config)

chambers/layers/embedding.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 
 
+@tf.keras.utils.register_keras_serializable(package="Chambers")
 class PositionalEmbedding1D(tf.keras.layers.Layer):
     def __init__(self, embedding_dim, temperature=10000, add_to_input=True, **kwargs):
         super(PositionalEmbedding1D, self).__init__(**kwargs)
@@ -73,6 +74,7 @@ class PositionalEmbedding1D(tf.keras.layers.Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+@tf.keras.utils.register_keras_serializable(package="Chambers")
 class PositionalEmbedding2D(tf.keras.layers.Layer):
     # These are the default parameters used in the original project
     def __init__(
@@ -162,6 +164,7 @@ class PositionalEmbedding2D(tf.keras.layers.Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+@tf.keras.utils.register_keras_serializable(package="Chambers")
 class LearnedEmbedding1D(tf.keras.layers.Layer):
     def __init__(
         self,
@@ -201,6 +204,7 @@ class LearnedEmbedding1D(tf.keras.layers.Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+@tf.keras.utils.register_keras_serializable(package="Chambers")
 class ConcatEmbedding(tf.keras.layers.Layer):
     def __init__(
         self,
@@ -255,13 +259,3 @@ class ConcatEmbedding(tf.keras.layers.Layer):
         }
         base_config = super(ConcatEmbedding, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-
-
-tf.keras.utils.get_custom_objects().update(
-    {
-        "PositionalEmbedding1D": PositionalEmbedding1D,
-        "PositionalEmbedding2D": PositionalEmbedding2D,
-        "LearnedEmbedding1D": LearnedEmbedding1D,
-        "ConcatEmbedding": ConcatEmbedding,
-    }
-)

chambers/models/transformer.py
@@ -28,7 +28,7 @@ def Seq2SeqTransformer(
         input_vocab_size, embed_dim, mask_zero=True, name="inputs_embed"
     )(inputs)
     x_enc = PositionalEmbedding1D(embed_dim, name="inputs_positional_encoding")(x_enc)
-    x = Encoder(
+    x_enc = Encoder(
         embed_dim=embed_dim,
         num_heads=num_heads,
         ff_dim=dim_feedforward,
@@ -41,7 +41,7 @@ def Seq2SeqTransformer(
         output_vocab_size, embed_dim, mask_zero=True, name="targets_embed"
     )(targets)
     x_dec = PositionalEmbedding1D(embed_dim, name="targets_positional_encoding")(x_dec)
-    x = Decoder(
+    x_dec = Decoder(
         embed_dim=embed_dim,
         num_heads=num_heads,
         ff_dim=dim_feedforward,
@@ -50,9 +50,9 @@ def Seq2SeqTransformer(
         dense_dropout_rate=dropout_rate,
         norm_output=False,
         causal=True,
-    )([x_dec, x])
+    )([x_dec, x_enc])
 
-    x = tf.keras.layers.Dense(output_vocab_size)(x)
+    x = tf.keras.layers.Dense(output_vocab_size)(x_dec)
 
     model = tf.keras.models.Model(inputs=[inputs, targets], outputs=x, name=name)
     return model
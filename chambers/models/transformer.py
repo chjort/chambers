5f169b412da4b9a0186364fddd4f1986d53d3cd2
chjort <christoffer.hjort1995@gmail.com>
2021-03-31 04:16:30 +0200

    refactor VisionTransformer

--- chambers/models/transformer.py
+++ chambers/models/transformer.py
@@ -1,13 +1,10 @@
 import tensorflow as tf
-from einops.layers.tensorflow import Rearrange
 from tensorflow.python.keras.applications import imagenet_utils
 
-from chambers.activations import gelu
 from chambers.layers.embedding import (
     PositionalEmbedding1D,
     ConcatEmbedding,
     LearnedEmbedding1D,
-    LearnedEmbedding0D,
 )
 from chambers.layers.transformer import Encoder, Decoder
 from chambers.utils.layer_utils import inputs_to_input_layer
@@ -67,29 +64,33 @@ def VisionTransformer(
     n_encoder_layers,
     n_heads,
     ff_dim,
-    feature_dim,
     dropout_rate=0.0,
     input_tensor=None,
     input_shape=None,
     include_top=True,
+    weights=None,
     pooling=None,
+    feature_dim=None,
     classes=1000,
     classifier_activation=None,
     name=None,
 ):
+    # TODO: validate that feature_dim and weights are mutually exclusive
+    # TODO: if weights are given and feature_dim is None, set appropriate feature_dim
+
     input_shape = imagenet_utils.obtain_input_shape(
         input_shape=input_shape,
         default_size=224,
         min_size=patch_size,
         data_format=tf.keras.backend.image_data_format(),
-        require_flatten=False,
-        weights=None,
+        require_flatten=include_top,
+        weights=weights,
     )
     inputs = inputs_to_input_layer(input_tensor, input_shape)
 
-    if inputs.shape[1] is None or inputs.shape[2] is None:
+    if None in input_shape:
         raise ValueError(
-            "Input height and width must be fully specified; got input shape {}.".format(
+            "'input_shape' must be fully specified; got 'input_shape'={}.".format(
                 inputs.shape[1:]
             )
         )
@@ -131,20 +132,24 @@ def VisionTransformer(
     )(x)
 
     if pooling == "avg":
+        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
         x = tf.keras.layers.GlobalAveragePooling1D(name="avg_pool")(x)
     elif pooling == "max":
+        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
         x = tf.keras.layers.GlobalMaxPooling1D(name="max_pool")(x)
     else:
         x = tf.keras.Sequential(
             [
-                tf.keras.layers.Lambda(lambda x: x[:, 0]),
+                tf.keras.layers.Cropping1D((0, x.shape[1] - 1)),
                 tf.keras.layers.Reshape([-1]),
             ],
             name="cls_embedding",
         )(x)
-        x = tf.keras.layers.Dense(
-            units=feature_dim, activation="tanh", name="cls_feature"
-        )(x)
+
+    if feature_dim is not None:
+        x = tf.keras.layers.Dense(units=feature_dim, activation="tanh", name="feature")(
+            x
+        )
 
     if include_top:
         x = tf.keras.layers.Dense(
@@ -152,91 +157,266 @@ def VisionTransformer(
         )(x)
 
     model = tf.keras.models.Model(inputs=inputs, outputs=x, name=name)
+
+    if weights:
+        # TODO: load pretrained weights
+
+        pass
+
     return model
 
 
-def VisionTransformerOS(
-    input_shape,
-    n_classes,
-    patch_size,
-    patch_dim,
-    n_encoder_layers,
-    n_heads,
-    ff_dim,
-    dropout_rate=0.0,
+def ViTB16(
+    input_tensor=None,
+    input_shape=None,
+    include_top=True,
+    weights=None,
+    pooling=None,
+    feature_dim=None,
+    classes=1000,
+    classifier_activation=None,
 ):
-    inputs1 = tf.keras.layers.Input(input_shape)
-    inputs2 = tf.keras.layers.Input(input_shape)
+    patch_size = 16
+    patch_dim = 768
 
-    patch_embeddings = tf.keras.Sequential(
-        [
-            Rearrange(
-                "b (h p1) (w p2) c -> b (h w) (p1 p2 c)", p1=patch_size, p2=patch_size
-            ),
-            tf.keras.layers.Dense(patch_dim),
-        ],
-        name="patch_embeddings",
+    # validate input
+    imagenet_utils.obtain_input_shape(
+        input_shape=input_shape,
+        default_size=224,
+        min_size=patch_size,
+        data_format=tf.keras.backend.image_data_format(),
+        require_flatten=include_top,
+        weights=weights,
     )
-    x1 = patch_embeddings(inputs1)
-    x2 = patch_embeddings(inputs2)
 
-    x1 = LearnedEmbedding0D(
-        initializer=tf.keras.initializers.RandomNormal(stddev=0.06),
-        name="segment1_embedding",
-    )(x1)
-    x2 = LearnedEmbedding0D(
-        initializer=tf.keras.initializers.RandomNormal(stddev=0.06),
-        name="segment2_embedding",
-    )(x2)
-
-    x = ConcatEmbedding(
-        n_embeddings=1,
-        embedding_dim=patch_dim,
-        side="left",
-        axis=1,
-        initializer="zeros",
-        name="add_cls_token",
-    )(x1)
-    x = ConcatEmbedding(
-        n_embeddings=1,
-        embedding_dim=patch_dim,
-        side="right",
-        axis=1,
-        initializer="zeros",
-        name="add_sep_token",
-    )(x)
-    x = tf.keras.layers.Concatenate(axis=1)([x, x2])
-
-    x = LearnedEmbedding1D(
-        initializer=tf.keras.initializers.RandomNormal(stddev=0.06),
-        name="pos_embedding",
-    )(x)
-    x = Encoder(
-        embed_dim=patch_dim,
-        num_heads=n_heads,
-        ff_dim=ff_dim,
-        num_layers=n_encoder_layers,
-        attention_dropout_rate=dropout_rate,
-        dense_dropout_rate=dropout_rate,
-        norm_output=True,
-    )(x)
-    x = tf.keras.layers.Cropping1D((0, x.shape[1] - 1))(x)
-    x = tf.keras.layers.Reshape([-1])(x)
-
-    x = tf.keras.Sequential(
-        [
-            tf.keras.layers.Dense(ff_dim, activation=gelu),
-            tf.keras.layers.Dense(n_classes, activation="sigmoid"),
-        ],
-        name="mlp_head",
-    )(x)
-
-    model = tf.keras.models.Model([inputs1, inputs2], x)
-    return model
+    vit = VisionTransformer(
+        patch_size=patch_size,
+        patch_dim=patch_dim,
+        n_encoder_layers=12,
+        n_heads=12,
+        ff_dim=3072,
+        dropout_rate=0.1,
+        feature_dim=feature_dim,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        include_top=include_top,
+        weights=weights,
+        pooling=pooling,
+        classes=classes,
+        classifier_activation=classifier_activation,
+        name="vitb16",
+    )
+    return vit
 
 
-tf.keras.utils.get_custom_objects().update(
-    {
-        "Rearrange": Rearrange,
-    }
-)
+def ViTB32(
+    input_tensor=None,
+    input_shape=None,
+    include_top=True,
+    weights=None,
+    pooling=None,
+    feature_dim=None,
+    classes=1000,
+    classifier_activation=None,
+):
+    patch_size = 32
+    patch_dim = 768
+
+    # validate input
+    imagenet_utils.obtain_input_shape(
+        input_shape=input_shape,
+        default_size=224,
+        min_size=patch_size,
+        data_format=tf.keras.backend.image_data_format(),
+        require_flatten=include_top,
+        weights=weights,
+    )
+
+    vit = VisionTransformer(
+        patch_size=patch_size,
+        patch_dim=patch_dim,
+        n_encoder_layers=12,
+        n_heads=12,
+        ff_dim=3072,
+        dropout_rate=0.1,
+        feature_dim=feature_dim,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        include_top=include_top,
+        weights=weights,
+        pooling=pooling,
+        classes=classes,
+        classifier_activation=classifier_activation,
+        name="vitb32",
+    )
+    return vit
+
+
+def ViTL16(
+    input_tensor=None,
+    input_shape=None,
+    include_top=True,
+    weights=None,
+    pooling=None,
+    feature_dim=None,
+    classes=1000,
+    classifier_activation=None,
+):
+    patch_size = 16
+    patch_dim = 1024
+
+    # validate input
+    imagenet_utils.obtain_input_shape(
+        input_shape=input_shape,
+        default_size=384,
+        min_size=patch_size,
+        data_format=tf.keras.backend.image_data_format(),
+        require_flatten=include_top,
+        weights=weights,
+    )
+
+    vit = VisionTransformer(
+        patch_size=patch_size,
+        patch_dim=patch_dim,
+        n_encoder_layers=24,
+        n_heads=16,
+        ff_dim=4096,
+        dropout_rate=0.1,
+        feature_dim=feature_dim,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        include_top=include_top,
+        weights=weights,
+        pooling=pooling,
+        classes=classes,
+        classifier_activation=classifier_activation,
+        name="vitl16",
+    )
+    return vit
+
+
+def ViTL32(
+    input_tensor=None,
+    input_shape=None,
+    include_top=True,
+    weights=None,
+    pooling=None,
+    feature_dim=None,
+    classes=1000,
+    classifier_activation=None,
+):
+    patch_size = 32
+    patch_dim = 1024
+
+    # validate input
+    imagenet_utils.obtain_input_shape(
+        input_shape=input_shape,
+        default_size=384,
+        min_size=patch_size,
+        data_format=tf.keras.backend.image_data_format(),
+        require_flatten=include_top,
+        weights=weights,
+    )
+
+    vit = VisionTransformer(
+        patch_size=patch_size,
+        patch_dim=patch_dim,
+        n_encoder_layers=24,
+        n_heads=16,
+        ff_dim=4096,
+        dropout_rate=0.1,
+        feature_dim=feature_dim,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        include_top=include_top,
+        weights=weights,
+        pooling=pooling,
+        classes=classes,
+        classifier_activation=classifier_activation,
+        name="vitl32",
+    )
+    return vit
+
+
+# def VisionTransformerOS(
+#     input_shape,
+#     n_classes,
+#     patch_size,
+#     patch_dim,
+#     n_encoder_layers,
+#     n_heads,
+#     ff_dim,
+#     dropout_rate=0.0,
+# ):
+#     inputs1 = tf.keras.layers.Input(input_shape)
+#     inputs2 = tf.keras.layers.Input(input_shape)
+#
+#     patch_embeddings = tf.keras.Sequential(
+#         [
+#             tf.keras.layers.Conv2D(
+#                 filters=patch_dim,
+#                 kernel_size=patch_size,
+#                 strides=patch_size,
+#                 padding="valid",
+#                 name="embedding",
+#             ),
+#             tf.keras.layers.Reshape([-1, patch_dim]),
+#         ],
+#         name="patch_embeddings",
+#     )
+#     x1 = patch_embeddings(inputs1)
+#     x2 = patch_embeddings(inputs2)
+#
+#     x1 = LearnedEmbedding0D(
+#         initializer=tf.keras.initializers.RandomNormal(stddev=0.06),
+#         name="segment1_embedding",
+#     )(x1)
+#     x2 = LearnedEmbedding0D(
+#         initializer=tf.keras.initializers.RandomNormal(stddev=0.06),
+#         name="segment2_embedding",
+#     )(x2)
+#
+#     x = ConcatEmbedding(
+#         n_embeddings=1,
+#         embedding_dim=patch_dim,
+#         side="left",
+#         axis=1,
+#         initializer="zeros",
+#         name="add_cls_token",
+#     )(x1)
+#     x = ConcatEmbedding(
+#         n_embeddings=1,
+#         embedding_dim=patch_dim,
+#         side="right",
+#         axis=1,
+#         initializer="zeros",
+#         name="add_sep_token",
+#     )(x)
+#     x = tf.keras.layers.Concatenate(axis=1)([x, x2])
+#
+#     x = LearnedEmbedding1D(
+#         initializer=tf.keras.initializers.RandomNormal(stddev=0.06),
+#         name="pos_embedding",
+#     )(x)
+#     x = Encoder(
+#         embed_dim=patch_dim,
+#         num_heads=n_heads,
+#         ff_dim=ff_dim,
+#         num_layers=n_encoder_layers,
+#         attention_dropout_rate=dropout_rate,
+#         dense_dropout_rate=dropout_rate,
+#         norm_output=True,
+#     )(x)
+#     x = tf.keras.layers.Cropping1D((0, x.shape[1] - 1))(x)
+#     x = tf.keras.layers.Reshape([-1])(x)
+#
+#     x = tf.keras.Sequential(
+#         [
+#             tf.keras.layers.Dense(ff_dim, activation=gelu),
+#             tf.keras.layers.Dense(n_classes, activation="sigmoid"),
+#         ],
+#         name="mlp_head",
+#     )(x)
+#
+#     model = tf.keras.models.Model([inputs1, inputs2], x)
+#     return model

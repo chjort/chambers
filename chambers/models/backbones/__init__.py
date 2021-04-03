dd89b364a153a7d8ed5e9a3c4f61b31472888851
chjort <christoffer.hjort1995@gmail.com>
2021-04-03 23:19:04 +0200

    WIP: pretrained weights for ViT models

--- chambers/models/backbones/__init__.py
+++ chambers/models/backbones/__init__.py
@@ -7,4 +7,11 @@ from .senet import SEResNet152
 from .senet import SEResNeXt50
 from .senet import SEResNeXt101
 from .senet import SENet154
+from .vision_transformer import ViTS16
+from .vision_transformer import ViTB16
+from .vision_transformer import ViTB32
+from .vision_transformer import ViTL16
+from .vision_transformer import ViTL32
+from .vision_transformer import DeiTS16
+from .vision_transformer import DeiTB16
 

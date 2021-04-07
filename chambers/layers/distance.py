88ba994ce2c1bd8df37857e1ee2e157d4f11d8c9
chjort <christoffer.hjort1995@gmail.com>
2021-04-07 23:53:00 +0200

    refactor distance layers

--- chambers/layers/distance.py
+++ chambers/layers/distance.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 
 class Distance(tf.keras.layers.Layer):
-    def __init__(self, axis=-1, keepdims=True, **kwargs):
+    def __init__(self, axis=-1, keepdims=False, **kwargs):
         super(Distance, self).__init__(**kwargs)
         self.axis = axis
         self.keepdims = keepdims
@@ -19,16 +19,16 @@ class L1Distance(Distance):
     """
     L1 distance or "Manhattan-distance" layer
 
-    This layer takes as input a list of two vectors [v1, v2] and computes
-    the L1 distance between v1 and v2 according to the following equation:
+    This layer takes as input a list of two vectors [a, b] and computes
+    the L1 distance between a and b according to the following equation:
 
-            l1 = |v1 - v2|
+            l1 = |a - b|
 
     """
 
     def call(self, inputs, **kwargs):
-        v1, v2 = inputs
-        x = v1 - v2
+        a, b = inputs
+        x = a - b
         x = tf.abs(x)
         x = tf.reduce_sum(x, axis=self.axis, keepdims=self.keepdims)
         return x
@@ -39,16 +39,16 @@ class L2Distance(Distance):
     """
     L2 distance layer. Also knows as Euclidean distance.
 
-    This layer takes as input a list of two vectors [v1, v2] and computes
-    the Euclidean distance between v1 and v2 according to the following equation:
+    This layer takes as input a list of two vectors [a, b] and computes
+    the Euclidean distance between a and b according to the following equation:
 
-            euclidean distance = sqrt((v1 - v2) . (v1 - v2))
+            euclidean distance = sqrt((a - b) . (a - b))
 
     """
 
     def call(self, inputs, **kwargs):
-        v1, v2 = inputs
-        x = v1 - v2
+        a, b = inputs
+        x = a - b
         x = tf.square(x)
         x = tf.reduce_sum(x, axis=self.axis, keepdims=self.keepdims)
         x = tf.sqrt(x)
@@ -60,12 +60,12 @@ class CosineSimilarity(Distance):
     """
     Cosine distance layer
 
-    This layer takes as input a list of two vectors [v1, v2] and computes
-    the Cosine distance between v1 and v2 according to the following equation:
+    This layer takes as input a list of two vectors [a, b] and computes
+    the cosine similarity between a and b according to the following equation:
 
-            cosine similarity = (v1 . v2) / (||v1|| * ||v2||)
+            cosine similarity = (a . b) / (||a|| * ||b||)
 
-            cosine distance = -cosine similarity
+            scaled cosine similarity = (cosine similairty + 1) / 2
 
     """
 
@@ -75,7 +75,6 @@ class CosineSimilarity(Distance):
         return self._scale(x)
 
     def _cosine_similarity(self, a, b):
-        tf.keras.losses.cosine_similarity
         a = tf.nn.l2_normalize(a, axis=self.axis)
         b = tf.nn.l2_normalize(b, axis=self.axis)
         x = a * b

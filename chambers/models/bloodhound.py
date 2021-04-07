55c8b0561910f4e3bbf47c7b807c6936756183f4
chjort <christoffer.hjort1995@gmail.com>
2021-04-07 03:09:44 +0200

    Bloodhound version

--- chambers/models/bloodhound.py
+++ chambers/models/bloodhound.py
@@ -7,27 +7,58 @@ from chambers.models.backbones.vision_transformer import _obtain_inputs
 from chambers.layers.normalization import L2Normalization
 
 
+def _pool(x, method=None, prefix=""):
+    if method == "avg":
+        x = tf.keras.layers.Cropping1D((1, 0), name=prefix + "sequence_embeddings")(x)
+        x = tf.keras.layers.GlobalAveragePooling1D(name=prefix + "avg_pool")(x)
+    elif method == "max":
+        x = tf.keras.layers.Cropping1D((1, 0), name=prefix + "sequence_embeddings")(x)
+        x = tf.keras.layers.GlobalMaxPooling1D(name=prefix + "max_pool")(x)
+    elif method == "sum":
+        x = tf.keras.layers.Cropping1D((1, 0), name=prefix + "sequence_embeddings")(x)
+        x = Sum(axis=1, name=prefix + "sum_pool")(x)
+    elif method == "cls":
+        x = tf.keras.Sequential(
+            [
+                tf.keras.layers.Cropping1D((0, x.shape[1] - 1)),
+                tf.keras.layers.Reshape([-1]),
+            ],
+            name=prefix + "cls_embedding",
+        )(x)
+
+    return x
+
+class Matmul(tf.keras.layers.Layer):
+
+    def compute_output_shape(self, input_shape):
+        shape_a, shape_b = input_shape
+        return [shape_a[0], shape_b[0]]
+
+    def call(self, inputs, **kwargs):
+        a, b = inputs
+        return tf.matmul(a, b, transpose_b=True)
+
+
 def Bloodhound0(
-    n_layers,
-    n_heads,
-    ff_dim,
-    dropout_rate=0.1,
-    input_tensor1=None,
-    input_shape1=None,
-    input_tensor2=None,
-    input_shape2=None,
-    include_top=True,
-    weights="imagenet21k+_224",
-    pooling=None,
-    feature_dim=None,
-    model_name=None,
+        n_layers,
+        n_heads,
+        ff_dim,
+        dropout_rate=0.1,
+        query_tensor=None,
+        query_shape=None,
+        candidates_tensor=None,
+        candidates_shape=None,
+        include_top=True,
+        weights="imagenet21k+_224",
+        pooling=None,
+        feature_dim=None,
+        model_name=None,
 ):
     """
-    c -> encoder ->
+    q -> encoder -> -----> zq
                   |
                   v
-          q -> decoder -> z (1d)
-
+          c -> decoder -> zc
     """
     enc = backbones.ViTB16(
         weights="imagenet21k+_224",
@@ -35,37 +66,37 @@ def Bloodhound0(
     )
     patch_size = enc.get_layer("patch_embeddings").get_layer("embedding").kernel_size
 
-    inputs1 = _obtain_inputs(
-        input_tensor1,
-        input_shape1,
+    inputs_q = _obtain_inputs(
+        query_tensor,
+        query_shape,
         default_size=224,
         min_size=patch_size,
         weights=weights,
         model_name=model_name,
-        name="x1",
+        name="query",
     )
-    inputs2 = _obtain_inputs(
-        input_tensor2,
-        input_shape2,
+    inputs_c = _obtain_inputs(
+        candidates_tensor,
+        candidates_shape,
         default_size=224,
         min_size=patch_size,
         weights=weights,
         model_name=model_name,
-        name="x2",
+        name="candidates",
     )
 
     enc = tf.keras.Model(
         inputs=enc.inputs, outputs=enc.get_layer("encoder").output, name="encoder"
     )
-    x_enc = enc(inputs1)
-
     embed = tf.keras.Model(
         inputs=enc.inputs, outputs=enc.get_layer("pos_embedding").output, name="embed"
     )
-    x = embed(inputs2)
-    x = tf.keras.layers.Dropout(dropout_rate)(x)
-    x = Decoder(
-        embed_dim=x.shape[-1],
+    q_enc = enc(inputs_q)
+    c_enc = embed(inputs_c)
+
+    x_c = tf.keras.layers.Dropout(dropout_rate)(c_enc)
+    x_c = Decoder(
+        embed_dim=x_c.shape[-1],
         num_heads=n_heads,
         ff_dim=ff_dim,
         num_layers=n_layers,
@@ -74,34 +105,28 @@ def Bloodhound0(
         norm_output=True,
         pre_norm=False,
         causal=False,
-    )([x, x_enc])
+    )([x_c, q_enc])
 
-    if pooling == "avg":
-        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
-        x = tf.keras.layers.GlobalAveragePooling1D(name="avg_pool")(x)
-    elif pooling == "max":
-        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
-        x = tf.keras.layers.GlobalMaxPooling1D(name="max_pool")(x)
-    elif pooling == "cls":
-        x = tf.keras.Sequential(
-            [
-                tf.keras.layers.Cropping1D((0, x.shape[1] - 1)),
-                tf.keras.layers.Reshape([-1]),
-            ],
-            name="cls_embedding",
-        )(x)
-    else:
-        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
-        x = Sum(axis=1, name="sum_pool")(x)
+    x_q = _pool(q_enc, method=pooling, prefix="q_")
+    x_c = _pool(x_c, method=pooling, prefix="c_")
 
     if include_top:
-        if feature_dim is None:
-            feature_dim = x.shape[-1]
+        if pooling is None:
+            raise ValueError("`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'.")
 
-        x = tf.keras.layers.Dense(feature_dim)(x)
-        x = L2Normalization(axis=-1)(x)
+        if feature_dim is not None:
+            x_q = tf.keras.layers.Dense(feature_dim)(x_q)
+            x_c = tf.keras.layers.Dense(feature_dim)(x_c)
 
-    model = tf.keras.Model(inputs=[inputs1, inputs2], outputs=x)
+        x_q = L2Normalization(axis=-1)(x_q)
+        x_c = L2Normalization(axis=-1)(x_c)
+        x = Matmul()([x_q, x_c])
+    else:
+        x = [x_q, x_c]
+
+    model = tf.keras.Model(
+        inputs=[inputs_q, inputs_c], outputs=x, name=model_name
+    )
     return model
 
 
@@ -121,11 +146,10 @@ def Bloodhound1(
     model_name=None,
 ):
     """
-        c -> encoder ->
+        q -> encoder -> -----> zq
                       |
                       v
-    q -> encoder -> decoder -> z (1d)
-
+    c -> encoder -> decoder -> zc
     """
     enc = backbones.ViTB16(
         weights="imagenet21k+_224",
@@ -158,8 +182,8 @@ def Bloodhound1(
     q_enc = enc(inputs_q)
     c_enc = enc(inputs_c)
     # TODO: positional embedding here too?
-    x = Decoder(
-        embed_dim=q_enc.shape[-1],
+    x_c = Decoder(
+        embed_dim=c_enc.shape[-1],
         num_heads=n_heads,
         ff_dim=ff_dim,
         num_layers=n_layers,
@@ -168,33 +192,26 @@ def Bloodhound1(
         norm_output=True,
         pre_norm=False,
         causal=False,
-    )([q_enc, c_enc])
+    )([c_enc, q_enc])
 
-    if pooling == "avg":
-        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
-        x = tf.keras.layers.GlobalAveragePooling1D(name="avg_pool")(x)
-    elif pooling == "max":
-        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
-        x = tf.keras.layers.GlobalMaxPooling1D(name="max_pool")(x)
-    elif pooling == "cls":
-        x = tf.keras.Sequential(
-            [
-                tf.keras.layers.Cropping1D((0, x.shape[1] - 1)),
-                tf.keras.layers.Reshape([-1]),
-            ],
-            name="cls_embedding",
-        )(x)
-    else:
-        x = tf.keras.layers.Cropping1D((1, 0), name="sequence_embeddings")(x)
-        x = Sum(axis=1, name="sum_pool")(x)
+    x_q = _pool(q_enc, method=pooling, prefix="q_")
+    x_c = _pool(x_c, method=pooling, prefix="c_")
 
     if include_top:
-        if feature_dim is None:
-            feature_dim = x.shape[-1]
+        if pooling is None:
+            raise ValueError("`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'.")
 
-        x = tf.keras.layers.Dense(feature_dim)(x)
-        # x = L2Normalization(axis=-1)(x)
-        x = tf.keras.layers.Dense(1, activation="sigmoid", dtype=tf.float32)(x)
+        if feature_dim is not None:
+            x_q = tf.keras.layers.Dense(feature_dim)(x_q)
+            x_c = tf.keras.layers.Dense(feature_dim)(x_c)
 
-    model = tf.keras.Model(inputs=[inputs_q, inputs_c], outputs=x)
+        x_q = L2Normalization(axis=-1)(x_q)
+        x_c = L2Normalization(axis=-1)(x_c)
+        x = Matmul()([x_q, x_c])
+    else:
+        x = [x_q, x_c]
+
+    model = tf.keras.Model(
+        inputs=[inputs_q, inputs_c], outputs=x, name=model_name
+    )
     return model

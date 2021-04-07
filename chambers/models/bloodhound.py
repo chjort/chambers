07b2b8aa131e370eebc149d161b2d034ce2d7d86
chjort <christoffer.hjort1995@gmail.com>
2021-04-08 00:12:55 +0200

    Bloodhound4D

--- chambers/models/bloodhound.py
+++ chambers/models/bloodhound.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 
+from chambers.layers.attention import MultiHeadAttention
 from chambers.layers.reduce import Sum
-from chambers.layers.transformer import Decoder
+from chambers.layers.transformer import Decoder, DecoderLayer
 from chambers.models import backbones
 from chambers.models.backbones.vision_transformer import _obtain_inputs
 from chambers.layers.normalization import L2Normalization
+from chambers.layers.distance import CosineSimilarity
 
 
 def _pool(x, method=None, prefix=""):
@@ -28,8 +30,54 @@ def _pool(x, method=None, prefix=""):
 
     return x
 
-class Matmul(tf.keras.layers.Layer):
 
+class _Pool3DAxis1(tf.keras.layers.Layer):
+    def __init__(self, method=None, keepdims=False, prefix=None):
+        name = method + "_pool" if method is not None else "identity"
+        name = prefix + name if prefix is not None else name
+
+        super(_Pool3DAxis1, self).__init__(name=name)
+        self.method = method
+        self.keepdims = keepdims
+        self.axis = 1
+
+    def call(self, inputs, **kwargs):
+        if self.method is None:
+            return inputs
+
+        x = self._slice(inputs)
+
+        if self.method == "avg":
+            x = tf.reduce_mean(x, axis=self.axis, keepdims=self.keepdims)
+        elif self.method == "max":
+            x = tf.reduce_max(x, axis=self.axis, keepdims=self.keepdims)
+        elif self.method == "sum":
+            x = tf.reduce_sum(x, axis=self.axis, keepdims=self.keepdims)
+
+        return x
+
+    def _slice(self, x):
+        if self.method == "cls":
+            x = x[:, 0, :]
+        else:
+            x = x[:, 1:, :]
+        return x
+
+
+class _Pool4DAxis2(_Pool3DAxis1):
+    def __init__(self, *args, **kwargs):
+        super(_Pool4DAxis2, self).__init__(*args, **kwargs)
+        self.axis = 2
+
+    def _slice(self, x):
+        if self.method == "cls":
+            x = x[:, :, 0, :]
+        else:
+            x = x[:, :, 1:, :]
+        return x
+
+
+class Matmul(tf.keras.layers.Layer):
     def compute_output_shape(self, input_shape):
         shape_a, shape_b = input_shape
         return [shape_a[0], shape_b[0]]
@@ -40,19 +88,19 @@ class Matmul(tf.keras.layers.Layer):
 
 
 def Bloodhound0(
-        n_layers,
-        n_heads,
-        ff_dim,
-        dropout_rate=0.1,
-        query_tensor=None,
-        query_shape=None,
-        candidates_tensor=None,
-        candidates_shape=None,
-        include_top=True,
-        weights="imagenet21k+_224",
-        pooling=None,
-        feature_dim=None,
-        model_name=None,
+    n_layers,
+    n_heads,
+    ff_dim,
+    dropout_rate=0.1,
+    query_tensor=None,
+    query_shape=None,
+    candidates_tensor=None,
+    candidates_shape=None,
+    include_top=True,
+    weights="imagenet21k+_224",
+    pooling=None,
+    feature_dim=None,
+    model_name=None,
 ):
     """
     q -> encoder -> -----> zq
@@ -107,12 +155,14 @@ def Bloodhound0(
         causal=False,
     )([x_c, q_enc])
 
-    x_q = _pool(q_enc, method=pooling, prefix="q_")
-    x_c = _pool(x_c, method=pooling, prefix="c_")
+    x_q = _Pool3DAxis1(method=pooling, prefix="q_")(q_enc)
+    x_c = _Pool3DAxis1(method=pooling, prefix="c_")(x_c)
 
     if include_top:
         if pooling is None:
-            raise ValueError("`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'.")
+            raise ValueError(
+                "`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'."
+            )
 
         if feature_dim is not None:
             x_q = tf.keras.layers.Dense(feature_dim)(x_q)
@@ -124,9 +174,7 @@ def Bloodhound0(
     else:
         x = [x_q, x_c]
 
-    model = tf.keras.Model(
-        inputs=[inputs_q, inputs_c], outputs=x, name=model_name
-    )
+    model = tf.keras.Model(inputs=[inputs_q, inputs_c], outputs=x, name=model_name)
     return model
 
 
@@ -194,12 +242,14 @@ def Bloodhound1(
         causal=False,
     )([c_enc, q_enc])
 
-    x_q = _pool(q_enc, method=pooling, prefix="q_")
-    x_c = _pool(x_c, method=pooling, prefix="c_")
+    x_q = _Pool3DAxis1(method=pooling, prefix="q_")(q_enc)
+    x_c = _Pool3DAxis1(method=pooling, prefix="c_")(x_c)
 
     if include_top:
         if pooling is None:
-            raise ValueError("`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'.")
+            raise ValueError(
+                "`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'."
+            )
 
         if feature_dim is not None:
             x_q = tf.keras.layers.Dense(feature_dim)(x_q)
@@ -211,7 +261,173 @@ def Bloodhound1(
     else:
         x = [x_q, x_c]
 
-    model = tf.keras.Model(
-        inputs=[inputs_q, inputs_c], outputs=x, name=model_name
+    model = tf.keras.Model(inputs=[inputs_q, inputs_c], outputs=x, name=model_name)
+    return model
+
+
+#%%
+
+
+class MultiHeadAttention4D(MultiHeadAttention):
+    def call(self, inputs, mask=None, training=None):
+        """
+        Einsum notation:
+        b = batch_size
+        e = expanded dimension
+        t = sequence length
+        d = embedding dimension
+        n = num heads
+        h = head dimension
+        """
+        q = inputs[0]  # [b, e, tq, d]
+        v = inputs[1]  # [e, b, tv, d]
+        k = inputs[2] if len(inputs) > 2 else v  # [e, b, tv, d]
+
+        # linear projections + head split
+        query = tf.einsum("ebtd,dnh->ebnth", q, self.w_query) + self.b_query
+        value = tf.einsum("betd,dnh->benth", v, self.w_value) + self.b_value
+        key = tf.einsum("betd,dnh->benth", k, self.w_key) + self.b_key
+
+        # TODO: Mask
+
+        attention = self.attention([query, value, key], mask=mask, training=training)
+
+        # linear projection + head merge
+        x = (
+            tf.einsum("benth,ndh->betd", attention, self.w_projection)
+            + self.b_projection
+        )
+
+        return x
+
+
+class DecoderLayer4D(DecoderLayer):
+    def __init__(self, *args, **kwargs):
+        super(DecoderLayer4D, self).__init__(*args, **kwargs)
+
+        # self-attention
+        self.multi_head_attention1 = MultiHeadAttention4D(
+            head_dim=self.embed_dim // self.num_heads,
+            num_heads=self.num_heads,
+            dense_kernel_initializer=self.dense_kernel_initializer,
+            dropout_rate=self.attention_dropout_rate,
+            causal=self.causal,
+        )
+
+        # cross-attention
+        self.multi_head_attention2 = MultiHeadAttention4D(
+            head_dim=self.embed_dim // self.num_heads,
+            num_heads=self.num_heads,
+            dense_kernel_initializer=self.dense_kernel_initializer,
+            dropout_rate=self.attention_dropout_rate,
+            causal=False,
+        )
+
+
+class Decoder4D(Decoder):
+    def build(self, input_shape):
+        self.layers = [
+            DecoderLayer4D(
+                embed_dim=self.embed_dim,
+                num_heads=self.num_heads,
+                ff_dim=self.ff_dim,
+                dense_kernel_initializer=self.dense_kernel_initializer,
+                attention_dropout_rate=self.attention_dropout_rate,
+                dense_dropout_rate=self.dense_dropout_rate,
+                norm_epsilon=self.norm_epsilon,
+                pre_norm=self.pre_norm,
+                causal=self.causal,
+            )
+            for i in range(self.num_layers)
+        ]
+
+
+def Bloodhound4D(
+    n_layers,
+    n_heads,
+    ff_dim,
+    dropout_rate=0.1,
+    query_tensor=None,
+    query_shape=None,
+    candidates_tensor=None,
+    candidates_shape=None,
+    include_top=True,
+    weights="imagenet21k+_224",
+    pooling=None,
+    feature_dim=None,
+    model_name=None,
+):
+    """
+        q -> encoder -> -----> zq
+                      |
+                      v
+    c -> encoder -> decoder -> zc
+    """
+    enc = backbones.ViTB16(
+        weights="imagenet21k+_224",
+        include_top=False,
     )
+    patch_size = enc.get_layer("patch_embeddings").get_layer("embedding").kernel_size
+
+    inputs_q = _obtain_inputs(
+        query_tensor,
+        query_shape,
+        default_size=224,
+        min_size=patch_size,
+        weights=weights,
+        model_name=model_name,
+        name="query",
+    )
+    inputs_c = _obtain_inputs(
+        candidates_tensor,
+        candidates_shape,
+        default_size=224,
+        min_size=patch_size,
+        weights=weights,
+        model_name=model_name,
+        name="candidates",
+    )
+
+    enc = tf.keras.Model(
+        inputs=enc.inputs, outputs=enc.get_layer("encoder").output, name="encoder"
+    )
+    q_enc = enc(inputs_q)
+    c_enc = enc(inputs_c)
+    # TODO: positional embedding here too?
+    q_enc = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, 1), name="q_expand")(
+        q_enc
+    )
+    c_enc = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, 0), name="c_expand")(
+        c_enc
+    )
+    x_c = Decoder4D(
+        embed_dim=c_enc.shape[-1],
+        num_heads=n_heads,
+        ff_dim=ff_dim,
+        num_layers=n_layers,
+        attention_dropout_rate=dropout_rate,
+        dense_dropout_rate=dropout_rate,
+        norm_output=True,
+        pre_norm=False,
+        causal=False,
+    )([c_enc, q_enc])
+
+    x_q = _Pool4DAxis2(method=pooling, prefix="q_")(q_enc)
+    x_c = _Pool4DAxis2(method=pooling, prefix="c_")(x_c)
+
+    if include_top:
+        if pooling is None:
+            raise ValueError(
+                "`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'."
+            )
+
+        if feature_dim is not None:
+            x_q = tf.keras.layers.Dense(feature_dim)(x_q)
+            x_c = tf.keras.layers.Dense(feature_dim)(x_c)
+
+        x = CosineSimilarity(axis=-1)([x_q, x_c])
+    else:
+        x = [x_q, x_c]
+
+    model = tf.keras.Model(inputs=[inputs_q, inputs_c], outputs=x, name=model_name)
     return model

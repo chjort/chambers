04f8483d4463a4630d3ec8d2a4e13459ae1ae891
chjort <christoffer.hjort1995@gmail.com>
2021-04-13 01:17:53 +0200

    clean up bloodhound.py

--- chambers/models/bloodhound.py
+++ chambers/models/bloodhound.py
@@ -1,10 +1,10 @@
 import math
 
 import tensorflow as tf
+from tensorflow.python.keras.utils import layer_utils
 
 from chambers.layers.attention import MultiHeadAttention
 from chambers.layers.distance import CosineSimilarity
-from chambers.layers.normalization import L2Normalization
 from chambers.layers.reduce import Sum
 from chambers.layers.transformer import Decoder, DecoderLayer
 from chambers.models import backbones
@@ -79,197 +79,6 @@ class _Pool4DAxis2(_Pool3DAxis1):
         else:
             x = x[:, :, 1:, :]
         return x
-
-
-class Matmul(tf.keras.layers.Layer):
-    def compute_output_shape(self, input_shape):
-        shape_a, shape_b = input_shape
-        return [shape_a[0], shape_b[0]]
-
-    def call(self, inputs, **kwargs):
-        a, b = inputs
-        return tf.matmul(a, b, transpose_b=True)
-
-
-def Bloodhound0(
-    n_layers,
-    n_heads,
-    ff_dim,
-    dropout_rate=0.1,
-    query_tensor=None,
-    query_shape=None,
-    candidates_tensor=None,
-    candidates_shape=None,
-    include_top=True,
-    weights="imagenet21k+_224",
-    pooling=None,
-    feature_dim=None,
-    model_name=None,
-):
-    """
-    q -> encoder -> -----> zq
-                  |
-                  v
-          c -> decoder -> zc
-    """
-    enc = backbones.ViTB16(
-        weights="imagenet21k+_224",
-        include_top=False,
-    )
-    patch_size = enc.get_layer("patch_embeddings").get_layer("embedding").kernel_size
-
-    inputs_q = _obtain_inputs(
-        query_tensor,
-        query_shape,
-        default_size=224,
-        min_size=patch_size,
-        weights=weights,
-        model_name=model_name,
-        name="query",
-    )
-    inputs_c = _obtain_inputs(
-        candidates_tensor,
-        candidates_shape,
-        default_size=224,
-        min_size=patch_size,
-        weights=weights,
-        model_name=model_name,
-        name="candidates",
-    )
-
-    enc = tf.keras.Model(
-        inputs=enc.inputs, outputs=enc.get_layer("encoder").output, name="encoder"
-    )
-    embed = tf.keras.Model(
-        inputs=enc.inputs, outputs=enc.get_layer("pos_embedding").output, name="embed"
-    )
-    q_enc = enc(inputs_q)
-    c_enc = embed(inputs_c)
-
-    x_c = tf.keras.layers.Dropout(dropout_rate)(c_enc)
-    x_c = Decoder(
-        embed_dim=x_c.shape[-1],
-        num_heads=n_heads,
-        ff_dim=ff_dim,
-        num_layers=n_layers,
-        attention_dropout_rate=dropout_rate,
-        dense_dropout_rate=dropout_rate,
-        norm_output=True,
-        pre_norm=False,
-        causal=False,
-    )([x_c, q_enc])
-
-    x_q = _Pool3DAxis1(method=pooling, prefix="q_")(q_enc)
-    x_c = _Pool3DAxis1(method=pooling, prefix="c_")(x_c)
-
-    if include_top:
-        if pooling is None:
-            raise ValueError(
-                "`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'."
-            )
-
-        if feature_dim is not None:
-            x_q = tf.keras.layers.Dense(feature_dim)(x_q)
-            x_c = tf.keras.layers.Dense(feature_dim)(x_c)
-
-        x_q = L2Normalization(axis=-1)(x_q)
-        x_c = L2Normalization(axis=-1)(x_c)
-        x = Matmul()([x_q, x_c])
-    else:
-        x = [x_q, x_c]
-
-    model = tf.keras.Model(inputs=[inputs_q, inputs_c], outputs=x, name=model_name)
-    return model
-
-
-def Bloodhound1(
-    n_layers,
-    n_heads,
-    ff_dim,
-    dropout_rate=0.1,
-    query_tensor=None,
-    query_shape=None,
-    candidates_tensor=None,
-    candidates_shape=None,
-    include_top=True,
-    weights="imagenet21k+_224",
-    pooling=None,
-    feature_dim=None,
-    model_name=None,
-):
-    """
-        q -> encoder -> -----> zq
-                      |
-                      v
-    c -> encoder -> decoder -> zc
-    """
-    enc = backbones.ViTB16(
-        weights="imagenet21k+_224",
-        include_top=False,
-    )
-    patch_size = enc.get_layer("patch_embeddings").get_layer("embedding").kernel_size
-
-    inputs_q = _obtain_inputs(
-        query_tensor,
-        query_shape,
-        default_size=224,
-        min_size=patch_size,
-        weights=weights,
-        model_name=model_name,
-        name="query",
-    )
-    inputs_c = _obtain_inputs(
-        candidates_tensor,
-        candidates_shape,
-        default_size=224,
-        min_size=patch_size,
-        weights=weights,
-        model_name=model_name,
-        name="candidates",
-    )
-
-    enc = tf.keras.Model(
-        inputs=enc.inputs, outputs=enc.get_layer("encoder").output, name="encoder"
-    )
-    q_enc = enc(inputs_q)
-    c_enc = enc(inputs_c)
-    # TODO: positional embedding here too?
-    x_c = Decoder(
-        embed_dim=c_enc.shape[-1],
-        num_heads=n_heads,
-        ff_dim=ff_dim,
-        num_layers=n_layers,
-        attention_dropout_rate=dropout_rate,
-        dense_dropout_rate=dropout_rate,
-        norm_output=True,
-        pre_norm=False,
-        causal=False,
-    )([c_enc, q_enc])
-
-    x_q = _Pool3DAxis1(method=pooling, prefix="q_")(q_enc)
-    x_c = _Pool3DAxis1(method=pooling, prefix="c_")(x_c)
-
-    if include_top:
-        if pooling is None:
-            raise ValueError(
-                "`include_top=True` requires `pooling` to be either 'avg', 'max', 'sum', or 'cls'."
-            )
-
-        if feature_dim is not None:
-            x_q = tf.keras.layers.Dense(feature_dim)(x_q)
-            x_c = tf.keras.layers.Dense(feature_dim)(x_c)
-
-        x_q = L2Normalization(axis=-1)(x_q)
-        x_c = L2Normalization(axis=-1)(x_c)
-        x = Matmul()([x_q, x_c])
-    else:
-        x = [x_q, x_c]
-
-    model = tf.keras.Model(inputs=[inputs_q, inputs_c], outputs=x, name=model_name)
-    return model
-
-
-#%%
 
 
 class MultiHeadAttention4D(MultiHeadAttention):
@@ -434,8 +243,29 @@ def Bloodhound4D(
     else:
         x = [x_q, x_c]
 
+    if query_tensor is not None:
+        inputs_q = layer_utils.get_source_inputs(query_tensor)
+    if candidates_tensor is not None:
+        inputs_c = layer_utils.get_source_inputs(candidates_tensor)
+
     model = tf.keras.Model(inputs=[inputs_q, inputs_c], outputs=x, name=model_name)
     return model
+
+
+def split_encoder_decoder(model):
+    enc = model.get_layer("encoder")
+
+    dec_inputs1 = tf.keras.layers.Input(shape=enc.output.shape[1:])
+    dec_inputs2 = tf.keras.layers.Input(shape=enc.output.shape[1:])
+    xq = model.get_layer("q_expand")(dec_inputs1)
+    xc = model.get_layer("c_expand")(dec_inputs2)
+    x = model.get_layer("decoder4d")([xc, xq])
+    xq = model.get_layer(index=-3)(xq)
+    xc = model.get_layer(index=-2)(x)
+    x = model.get_layer("cosine_similarity")([xq, xc])
+    dec = tf.keras.Model(inputs=[dec_inputs1, dec_inputs2], outputs=x)
+
+    return enc, dec
 
 
 #%%
@@ -516,55 +346,6 @@ def batch_predict(model, q, c, bq, bc=None, yq=None, yc=None, nq=None, nc=None, 
 
         yz = tf.cast(tf.equal(yq, tf.transpose(yc)), tf.int32)
 
-        return z, yz
-
-    return z
-
-
-def batch_predict2(model, q, c, bq, bc, yq=None, yc=None, nq=None, nc=None):
-    model = PredictDataModel.from_model(model)
-
-    if (yq is None and yc is not None) or (yc is None and yq is not None):
-        raise ValueError("Must have both `yq` and `yc` or have both be None.")
-
-    qd, nq = _to_dataset(q, yq, nq)
-    cd, nc = _to_dataset(c, yc, nc)
-    with_labels = not isinstance(qd.element_spec, tf.TensorSpec)
-
-    if bc is None:
-        bc = bq
-    qd = qd.batch(bq)
-    cd = cd.batch(bc)
-
-    nqb = math.ceil(nq / bq)
-    ncb = math.ceil(nc / bc)
-
-    z = []
-    yq = []
-    for batch in qd:
-        if with_labels:
-            qi, yqi = batch
-            yq.append(yqi)
-        else:
-            qi = batch
-
-        qi = tf.data.Dataset.from_tensors(qi).repeat(ncb)
-
-        if with_labels:
-            qi_c = tf.data.Dataset.zip((qi, cd)).map(lambda q, c: ((q, c[0]), c[1]))
-            zi, yc = model.predict(qi_c)
-        else:
-            qi_c = tf.data.Dataset.zip(((qi, cd),))
-            zi = model.predict(qi_c)
-
-        zi = tf.concat(tf.split(zi, nqb), axis=-1)  # TODO: Error when bq=2, bc=5, nq=10, nc=10
-        z.append(zi)
-    z = tf.concat(z, axis=0)
-
-    if with_labels:
-        yq = tf.concat(yq, axis=0)
-        yq = tf.expand_dims(yq, 1)
-        yz = tf.cast(tf.equal(yq, tf.transpose(yc)), tf.int32)
         return z, yz
 
     return z

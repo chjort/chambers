ee9d761a7525b9cd1d9da32cbde5d925a4714a97
chjort <christoffer.hjort1995@gmail.com>
2021-09-12 16:42:41 +0200

    inherit from keras Attention layer

--- chambers/layers/attention.py
+++ chambers/layers/attention.py
@@ -1,64 +1,10 @@
 import math
 
 import tensorflow as tf
-from keras.utils.control_flow_util import smart_cond
-from packaging import version
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import math_ops
 
 
 @tf.keras.utils.register_keras_serializable(package="Chambers")
-class Attention(tf.keras.layers.Attention):
-    def _apply_scores(self, scores, value, scores_mask=None, training=None):
-        """Applies attention scores to the given value tensor.
-
-        To use this method in your attention layer, follow the steps:
-
-        * Use `query` tensor of shape `[batch_size, Tq]` and `key` tensor of shape
-          `[batch_size, Tv]` to calculate the attention `scores`.
-        * Pass `scores` and `value` tensors to this method. The method applies
-          `scores_mask`, calculates `attention_distribution = softmax(scores)`, then
-          returns `matmul(attention_distribution, value).
-        * Apply `query_mask` and return the result.
-
-        Args:
-          scores: Scores float tensor of shape `[batch_size, Tq, Tv]`.
-          value: Value tensor of shape `[batch_size, Tv, dim]`.
-          scores_mask: A boolean mask `Tensor` of shape `[batch_size, 1, Tv]` or
-            `[batch_size, Tq, Tv]`. If given, scores at positions where
-            `scores_mask==False` do not contribute to the result. It must contain
-            at least one `True` value in each line along the last dimension.
-          training: Python boolean indicating whether the layer should behave in
-            training mode (adding dropout) or in inference mode (no dropout).
-
-        Returns:
-          Tensor of shape `[batch_size, Tq, dim]`.
-        """
-        if scores_mask is not None:
-            padding_mask = math_ops.logical_not(scores_mask)
-            # Bias so padding positions do not contribute to attention distribution.
-            scores -= tf.minimum(scores.dtype.max, 1.0e9) * math_ops.cast(
-                padding_mask, dtype=scores.dtype
-            )  # compatible with mixed precision
-        if training is None:
-            training = tf.keras.backend.learning_phase()
-        weights = tf.nn.softmax(scores)
-
-        def dropped_weights():
-            return tf.nn.dropout(weights, rate=self.dropout)
-
-        weights = smart_cond(
-            training, dropped_weights, lambda: array_ops.identity(weights)
-        )
-
-        if version.parse(tf.__version__) < version.parse("2.4"):
-            return math_ops.matmul(weights, value)
-        else:
-            return math_ops.matmul(weights, value), weights
-
-
-@tf.keras.utils.register_keras_serializable(package="Chambers")
-class ScaledAttention(Attention):
+class ScaledAttention(tf.keras.layers.Attention):
     def __init__(self, key_dim=None, *args, **kwargs):
         super(ScaledAttention, self).__init__(*args, **kwargs)
         self.key_dim = key_dim
